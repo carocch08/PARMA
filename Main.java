0ab885dc7aa6f09081f320907b1457e64c451503
carocch08 <caro.camposch@gmail.com>
2017-03-05 10:25:55 -0600

    Modificación Time

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
+import java.text.Normalizer.Form;
 
 public class Main {
 	
 	public static void main (String [ ] args) {
 	 
-	GaussianFilter gau = new GaussianFilter();
-	Time time = new Time();
-	time.time_ejecution();
+//	GaussianFilter gau = new GaussianFilter();
+//	Time time = new Time();
+//	time.time_ejecution();	
 	}
 
 }
--- Time.java
+++ Time.java
@@ -3,7 +3,6 @@ public class Time {
 
 	public void time_ejecution()
 	{
-		GaussianFilter gau = new GaussianFilter();
 		long time_start, time_end;
 		time_start = System.currentTimeMillis();
 		time_end = System.currentTimeMillis();
