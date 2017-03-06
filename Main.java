f913cbe85347b21ec1e481aaaf9d2cd85add7860
carocch08 <caro.camposch@gmail.com>
2017-03-05 23:43:38 -0600

    Sirve EQ Histogramas

--- Main.java
+++ Main.java
@@ -1,28 +1,26 @@
 import java.awt.image.BufferedImage;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.text.Normalizer.Form;
 
 import javax.imageio.stream.ImageInputStream;
 
 import org.omg.CORBA.portable.InputStream;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
 
 import javafx.scene.effect.ImageInput;
+import sun.nio.cs.HistoricallyNamedCharset;
 
 public class Main {
 	
-	public static void main (String [ ] args) {
-	 
-//	GaussianFilter gau = new GaussianFilter();
-//	Time time = new Time();
-//	time.time_ejecution();	
-		InputStream input = new FileInputStream('Captura2.png');
-		ImageInputStream imageInput = ImagenIO.createInputStream(input);
-		BufferedImage imagenL = ImagenIO.read(imageInput);
-		
-		int alto = imagenL.getHeight();
-		int ancho = imagenL.getWidth();
-		
-		System.out.println("Alto: " + alto + "Ancho: " + ancho);
+	
+	 public static void main( String[] args ) throws IOException
+	 {
+		HistogramsEQ hEQ = new HistogramsEQ();
+		hEQ.equalize_hist("Captura.jpg");	
 	}
 
 }
