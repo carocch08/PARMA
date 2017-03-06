44be5bb0d4ab868159a723e51de79bc886bee289
carocch08 <caro.camposch@gmail.com>
2017-03-06 13:48:56 -0600

    Correcciones HistoEQ

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main{
 	
 	public static void main( String[] args )
 	 {
-		Convolution c = new Convolution();
-		c.make_convolution("Captura2.jpg");
+//		Convolution c = new Convolution();
+//		c.make_convolution("Captura2.jpg");
 		
 		HistoEQ hEQ = new HistoEQ();
 		hEQ.eqHisto("Captura2.jpg");
