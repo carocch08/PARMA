62edc0a77f13fcaae632f94a89cdff590847b63d
carocch08 <caro.camposch@gmail.com>
2017-04-25 00:01:22 -0600

    LoadImages added

--- src/LoadImages.java
+++ src/LoadImages.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class LoadImages {
-	public void agregar_imagenes(String image_location)
+	public void add_images(String image_location)
 	{
 		ArrayList<String> images = new ArrayList<String>();
 		images.add(image_location);
